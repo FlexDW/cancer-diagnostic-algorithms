dc56933761eb26af538630bf4250289b16cc9889
FlexDW <felixward@gmail.com>
2016-11-14 12:53:20 +0100

    Update 5a-obtain-group-weights-PRAD.R

--- 5a-obtain-group-weights-PRAD.R
+++ 5a-obtain-group-weights-PRAD.R
@@ -14,7 +14,7 @@ mirChromo <- mirChromo[lapply(mirChromo, length) > 20]
 # Distributional partitions
 mirNorm <- sweep(PRAD$mirRaw, 2, PRAD$mirNF, "/")
 means <- rowMeans(mirNorm)
-capture.output(mirCounts <- CreatePartition(means, ngroup=6), file="GRridge_out/out.txt", append=FALSE)
+capture.output(mirCounts <- CreatePartition(means, ngroup=6), file="GRridge_out/PRAD_group_weights_out", append=FALSE)
 
 # Create partitions list
 parts <- list(mirCounts=mirCounts, 
@@ -31,7 +31,7 @@ capture.output(grro <- grridge(highdimdata=PRAD$mirDat,
                                innfold=5,
                                compareEN=TRUE,
                                maxsel=nvars.PRAD,
-                               trace=FALSE), file="GRridge_out/out.txt", append=TRUE)
+                               trace=FALSE), file="GRridge_out/PRAD_group_weights_out", append=TRUE)
 
 # Add values to list and remove old objects
 PRAD$grro <- grro
