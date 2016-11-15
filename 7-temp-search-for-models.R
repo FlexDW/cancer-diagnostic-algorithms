eac8088fec18e1a29f446160a0e97839d7d332c9
FlexDW <felixward@gmail.com>
2016-11-15 17:47:58 +0100

    Update 7-temp-search-for-models.R

--- 7-temp-search-for-models.R
+++ 7-temp-search-for-models.R
@@ -8,45 +8,54 @@ X <- t(PCBL$mirDat)
 Y <- as.numeric(!PCBL$ctrlIndex)
 cvSets <- getCvSets(y=Y, nsets=3, seed=cvSeed, print=FALSE)
 
-# Get glmnet formulated optimal L2 
-cvo <- cv.glmnet(x=X, y=Y, alpha=0, family="binomial", foldid=cvSets)
-L2 <- cvo$lambda.min/2
+# select weights
+weights <- seq(0.05, 0.95, length=19)
 
-# Set nvars to match Group Regularized EN (sometimes slightly different to target nvars)
-nvars.PCBL <- length(PCBL$grro1$resEN$whichEN)
-
-weights <- seq(0.01, 0.99, length=99)
-# setup parallel computing
+# do parallel
 cl <- makeCluster(2)
 registerDoParallel(cl)
-registerDoParallel(cores=3)
-
-# use iterator and run loops in parallel 'dopar'
+registerDoParallel(cores=4)
 iW <- iter(weights)
 searchres <- foreach(w=iW, .packages='glmnet') %dopar% {
-  #vars.lasso <- whichSel(x=X, y=Y, nvars=nvars.PCBL, w=abs(w - Y)^0.5)
-  vars.GRlasso <- whichSel(x=PCBL$grro1$XMw0, y=Y, nvars=nvars.PCBL, standardize=FALSE, w=abs(w - Y)^0.5)#, pf=PCBL$grro1$lambdamultvec[, 2])
-  #vars.GREN <- whichSel(x=PCBL$grro1$XMw0, y=Y, nvars=nvars.PCBL, L2=cvo$lambda.min/2, standardize=FALSE, w=abs(w - Y)^0.5)
-  #list(lasso=vars.lasso, GRlasso=vars.GRlasso, GREN=vars.GREN)
+  ridge <- whichSel(x=cbind(1, t(PCBL$mirDat)), 
+                    y=Y, 
+                    nvars=PCBL$nvars, 
+                    foldid=cvSets, 
+                    alpha=NULL, 
+                    lambda=NULL, 
+                    len=1000, 
+                    w=abs(1 - w - Y)^0.5, 
+                    pf=c(0, rep(1, ncol(PCBL$grro1$XMw0))),
+                    intercept=FALSE,
+                    maxit=10)
+  GRR <- whichSel(x=cbind(1, PCBL$grro1$XMw0), 
+                  y=Y, 
+                  nvars=PCBL$nvars, 
+                  foldid=cvSets, 
+                  alpha=NULL, 
+                  lambda=NULL, 
+                  len=1000, 
+                  w=abs(1 - w - Y)^0.5, 
+                  pf=c(0, rep(1, ncol(PCBL$grro1$XMw0))),
+                  intercept=FALSE,
+                  maxit=10)
+  list(ridge=ridge, GRR=GRR)
 }
 stopCluster(cl)
 
-res.lasso <- do.call(rbind, lapply(searchres, function(x) x$lasso))
-res.GRlasso <- do.call(rbind, lapply(searchres, function(x) x$GRlasso))
-res.GRlasso2 <- do.call(rbind, searchres)
-res.GREN <- do.call(rbind, lapply(searchres, function(x) x$GREN))
+ridge_models <- lapply(searchres, function(x) x[["ridge"]])
+GRR_models <- lapply(searchres, function(x) x[["GRR"]])
 
-unique.lasso <- res.lasso[!duplicated(res.lasso),]
-unique.GRlasso <- res.GRlasso[!duplicated(res.GRlasso),]
-unique.GRlasso2 <- res.GRlasso2[!duplicated(res.GRlasso2),]
-unique.GREN <- res.GREN[!duplicated(res.GREN),]
+ridge_models <- do.call(rbind, ridge_models)
+GRR_models <- do.call(rbind, GRR_models)
 
-unique.models <- rbind(res.lasso, res.GRlasso, res.GREN)
-unique.models <- unique.models[!duplicated(unique.models), ]
+ridge_models <- cbind(ridge_models, weights)
+GRR_models <- cbind(GRR_models, weights)
 
-PCBL$grro1$resEN$whichEN
+ridge_models <- ridge_models[rowSums(ridge_models[, 1:5]) > 0, ]
+GRR_models <- GRR_models[rowSums(GRR_models[, 1:5]) > 0, ]
 
-colnames(results) <- c("lambda", "w", "AUC", "sd")
+ridge_unique <- ridge_models[!duplicated(ridge_models[, 1:5]), 1:5, drop=FALSE]
+GRR_unique <- GRR_models[!duplicated(GRR_models[, 1:5]), 1:5, drop=FALSE]
 
-
-# rm(cols, X, Y, cvSets, cvo, L2, p.ridge, auc.ridge, roc.ridge, sens.ridge, vars.EN, p.EN, auc.EN, roc.EN, sens.EN, vars.lasso, p.lasso, auc.lasso, roc.lasso, sens.lasso, vars.GREN, p.GREN, auc.GREN, roc.GREN, sens.GREN, vars.GREN2, p.GREN2, auc.GREN2, roc.GREN2, sens.GREN2, legtext)
+# rm()
