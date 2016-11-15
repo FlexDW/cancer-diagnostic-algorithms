6aecb77330d9e42d6684eb4e666a042ae786fbd4
FlexDW <felixward@gmail.com>
2016-11-15 17:41:31 +0100

    Create _fn_whichSel_new.R

---
+++ _fn_whichSel_new.R
@@ -0,0 +1,71 @@
+whichSel <- function(x, y, nvars, foldid, alpha=NULL, lambda=NULL, len=1000, maxit=10, w=rep(1, nrow(x)), pf=rep(1, ncol(x)), intercept=TRUE){
+  if(is.null(alpha)){
+    if(is.null(lambda)){
+      cvo <- cv.glmnet(x, y, alpha=0, nlambda=600, standardize=FALSE, family="binomial", foldid=foldid, weights=w, penalty.factor=pf, intercept=intercept)
+      lambda <- cvo$lambda.min
+    }
+    
+    # create search grid
+    alpha <- seq(0.00000001, 0.99999999, length=len)
+    grid <- cbind(lambda/(1 - alpha), alpha)
+
+    # initialize loop parameters and begin search
+    lower <- 1; upper <- nrow(grid); success <- FALSE; count <- 0;
+    while(!success){
+      i <- round((lower + upper)/2)
+      glmo <- glmnet(x=x, y=y, lambda=grid[i, 1], alpha=grid[i, 2], standardize=FALSE, family="binomial", weights=w, penalty.factor=pf, intercept=intercept)
+      n_nonzero_feats <- sum(glmo$beta > 0)
+      if(n_nonzero_feats > nvars){
+        lower <- i
+      }else if(n_nonzero_feats < nvars){
+        upper <- i
+      }else if(n_nonzero_feats == nvars){
+        success <- TRUE
+        betas <- glmo$beta
+      }
+      if(abs(upper - lower) == 1 & !success){ # refine search area if not found
+        alpha <- seq(alpha[lower], alpha[upper], length=len)
+        grid <- cbind(lambda/(1 - alpha), alpha)
+        lower <- 1
+        upper <- nrow(grid)    
+      }
+      if(count > maxit){
+        success <- TRUE
+        betas <- glmo$beta
+      }
+    }
+  }else{
+    count <- 0
+    glmo <- glmnet(x=x, y=y, alpha=1, lambda=lambda, standardize=FALSE, family="binomial", weights=w, penalty.factor=pf, intercept=intercept)
+    lambda <- seq(0, max(glmo$lambda), length=len)
+    lower <- 1
+    upper <- length(lambda)    
+    success <- FALSE
+    while(!success){
+      i <- round((lower + upper)/2)
+      glmo <- glmnet(x=x, y=y, alpha=1, lambda=lambda[i], weights=w, penalty.factor=pf, standardize=standardize, intercept=intercept, type.logistic=type, family="binomial")
+      n_nonzero_feats <- sum(glmo$beta > 0)
+      if(n_nonzero_feats > nvars){
+        lower <- i
+      }else if(n_nonzero_feats < nvars){
+        upper <- i
+      }else if(n_nonzero_feats == nvars){
+        success <- TRUE
+        betas <- glmo$beta
+      }      
+      if(abs(upper - lower) == 1 & !success){
+        count <- count + 1
+        lambda <- seq(lambda[lower], lambda[upper], length.out=len)
+        lower <- 1
+        upper <- length(lambda)    
+      }
+      if(count > maxit){
+        success <- TRUE
+        betas <- glmo$beta
+      }
+    }
+  }
+  return(which(betas > 0))
+}
+
+# rm()
