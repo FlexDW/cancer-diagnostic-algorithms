890bb933ac3b3ffc91c859d32fdb5022cbcc3874
FlexDW <felixward@gmail.com>
2016-11-16 14:38:48 +0100

    Update _fn-cv.predict.R

--- _fn-cv.predict.R
+++ _fn-cv.predict.R
@@ -1,7 +1,7 @@
-cv.predict <- function(x, y, alpha, lambda, foldid, family="binomial", penalty.factor = rep(1, ncol(x)), standarize=TRUE){
+cv.predict <- function(x, y, alpha, lambda, foldid, family="binomial", penalty.factor = rep(1, ncol(x)), standardize=TRUE){
   
   if(is.null(lambda)){
-    cvo <- cv.glmnet(x=x, y=y, alpha=0, family="binomial", foldid=foldid, penalty.factor=penalty.factor, standarize=standarize)
+    cvo <- cv.glmnet(x=x, y=y, alpha=0, family="binomial", foldid=foldid, penalty.factor=penalty.factor, standardize=standardize)
     lambda <- cvo$lambda.min
   }
   
@@ -9,7 +9,7 @@ cv.predict <- function(x, y, alpha, lambda, foldid, family="binomial", penalty.f
   for(i in sort(unique(foldid))){
     train <- foldid != i
     test <- foldid == i
-    glmo <- glmnet(x=x[train, ], y=y[train], alpha=alpha, lambda=lambda, family=family, penalty.factor=penalty.factor, standarize=standarize)
+    glmo <- glmnet(x=x[train, ], y=y[train], alpha=alpha, lambda=lambda, family=family, penalty.factor=penalty.factor, standardize=standardize)
     p[test] <- predict(glmo, newx=x[test, ], type="response")
   }
   return(p)
