1dd7d547f7156848bda88e821a2b69e38dde9a71
FlexDW <felixward@gmail.com>
2016-11-23 12:08:33 +0100

    Update 5.2b-obtain-group-weights-PCBL-isomirs.R

--- 5.2b-obtain-group-weights-PCBL-isomirs.R
+++ 5.2b-obtain-group-weights-PCBL-isomirs.R
@@ -17,9 +17,9 @@ matched_betas <- matched_betas[complete.cases(matched_betas), ]
 capture.output(beta_parts_mirs <- CreatePartition(matched_betas$betas, ngroup=10), file="GRridge_out/PCBL_group_weights_out.txt", append=TRUE)
 beta_parts_iso <- lapply(beta_parts_mirs, function(x){ 
   mirs <- PCBL$mirs[matched_betas[x, 1]]
-  mir_table_index <- mir_table$miRNA_ID %in% mirs
-  which(PCBL$isomirs %in% mir_table$isoform_coords[mir_table_index])
-  })
+  mir_table_index <- PCBL$mirLookup$miRNA_ID %in% mirs
+  which(PCBL$isomirs %in% PCBL$mirLookup$isoform_coords[mir_table_index])
+})
 
 # Create partitions list
 parts1 <- list(beta_parts_iso=beta_parts_iso,
@@ -68,5 +68,5 @@ PCBL$iso_grro0 <- grro0
 PCBL$iso_grro1 <- grro1
 PCBL$iso_grro2 <- grro2
 
-rm(iso_normed, means, iso_counts, matched_betas, beta_parts_mirs, beta_parts_iso, mirs, mir_table_index, parts1, parts2, grro0, grro1, grro2)
+rm(iso_normed, means, iso_counts, matched_betas, beta_parts_mirs, beta_parts_iso, parts1, parts2, grro0, grro1, grro2)
 #rm(mirDGENorm, MM, mirDCRNorm, mirResGLMfitCRNorm, mirDENorm, matched.pvals, pvals.parts, parts3, grro3)
