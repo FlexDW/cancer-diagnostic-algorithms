7d4facdc174c417cecb905859233b9de8b9ce22a
FlexDW <felixward@gmail.com>
2016-12-12 15:01:11 +0100

    Update 6.2b-compare-ROC-sparse-models-PCBL-isomirs.R

--- 6.2b-compare-ROC-sparse-models-PCBL-isomirs.R
+++ 6.2b-compare-ROC-sparse-models-PCBL-isomirs.R
@@ -7,8 +7,8 @@
 # - "Diagrams/compare_ROC_PCBL_tissue_beta_groups.png"
 
 # set plot color vector
-cols <- c("red", "darkgreen", "green", "purple", "gold", "darkgrey")
-names(cols) <- c("ridge", "GRR", "EN", "lasso", "GREN", "GREN2")
+cols <- c("red", "gold", "darkgrey", "green", "purple", "gold", "darkgrey")
+names(cols) <- c("ridge", "GRR", "GRR2", "EN", "lasso", "GREN", "GREN2")
 
 # temporarily assign data
 X <- t(PCBL$isoDat)
@@ -58,40 +58,6 @@ auc.GREN <- glmnet::auc(prob=p.GREN, y=Y)
 roc.GREN <- t(GRridge::roc(probs=p.GREN, true=Y, cutoffs=seq(1, 0, length=201)))[, 1:2]
 sens.GREN <- sensitivity(p=p.GREN, y=Y, specificity=0.9)
 
-# create plot (all vars)
-png(filename="Diagrams/compare_ROC_PCBL_iso.png", width=800, height=400)
-par(mfrow=c(1, 2))
-plot(roc.ridge, type='l', col=cols["ridge"], 
-     main="ROC comparison, blood data (all variables)")
-points(roc.GRR, type='l', col=cols["GRR"])
-legtext <- paste(c("Ridge, ", "Group-regularized Ridge, "),
-                 "AUC: ",
-                 round(c(auc.ridge, auc.GRR), 3),
-                 ", Sens: ",
-                 round(c(sens.ridge, sens.GRR), 3),
-                 sep="")
-legend("bottomright", cex=0.8, lty=1,
-       legend=legtext,
-       col=cols[c("ridge", "GRR")],
-       title="AUC, Sensitivity at specificity=0.9")
-
-# create plot (5 vars)
-plot(roc.EN, type='l', col=cols["EN"], 
-     main="ROC comparison, blood data (variables: "%+%nvars.PCBL%+%")")
-points(roc.lasso, type='l', col=cols["lasso"])
-points(roc.GREN, type='l', col=cols["GREN"])
-legtext <- paste(c("Elastic Net, ", "Lasso, ", "Group Reg. EN, "),
-                 "AUC: ",
-                 round(c(auc.EN, auc.lasso, auc.GREN), 3),
-                 ", Sens: ",
-                 round(c(sens.EN, sens.lasso, sens.GREN), 3),
-                 sep="")
-legend("bottomright", cex=0.8, lty=1,
-       legend=legtext,
-       col=cols[c("EN", "lasso", "GREN")],
-       title="AUC, Sensitivity at specificity=0.9")
-dev.off()
-
 # Group regularized Ridge without Tissue Betas
 p.GRR2 <- cv.predict(x=cbind(1, PCBL$iso_grro2$XMw0), y=Y, alpha=0, lambda=cvo$lambda.min, foldid=cvSets, standardize=FALSE, intercept=FALSE, penalty.factor=rep(1, ncol(PCBL$iso_grro0$XMw0)))
 auc.GRR2 <- glmnet::auc(prob=p.GRR2, y=Y)
@@ -105,35 +71,71 @@ auc.GREN2 <- glmnet::auc(prob=p.GREN2, y=Y)
 roc.GREN2 <- t(GRridge::roc(probs=p.GREN2, true=Y, cutoffs=seq(1, 0, length=201)))[, 1:2]
 sens.GREN2 <- sensitivity(p=p.GREN2, y=Y, specificity=0.9)
 
-png(filename="Diagrams/compare_ROC_PCBL_iso_with_without_tissue_beta_groups.png", width=800, height=400)
+# create plot (all vars)
+png(filename="Diagrams/compare_ROC_PCBL_iso.png", width=800, height=400)
 par(mfrow=c(1, 2))
-plot(roc.GRR, type='l', col=cols["GREN"], #lwd=2,
-     main="Group Reg.with/without tissue betas (All Variables)")
-points(roc.GRR2, type='l', col=cols["GREN2"])
-legtext <- paste(c("With tissue betas, ", "Without, "),
+plot(roc.ridge, type='l', col=cols["ridge"], 
+     main="ROC comparison, blood data (all variables)")
+points(roc.GRR2, type='l', col=cols["GRR2"])
+points(roc.GRR, type='l', col=cols["GRR"])
+legtext <- paste(c("Ridge, ", "Grp Reg. Ridge (counts only), ", "Grp Reg. Ridge (+ tissue betas), "),
                  "AUC: ",
-                 round(c(auc.GRR, auc.GRR2), 3),
+                 round(c(auc.ridge, auc.GRR2, auc.GRR), 2),
                  ", Sens: ",
-                 round(c(sens.GRR, sens.GRR2), 3),
+                 round(c(sens.ridge, sens.GRR2, sens.GRR), 2),
                  sep="")
 legend("bottomright", cex=0.8, lty=1,
        legend=legtext,
-       col=cols[c("GREN", "GREN2")],
+       col=cols[c("ridge", "GRR2", "GRR")],
        title="AUC, Sensitivity at specificity=0.9")
 
-plot(roc.GREN, type='l', col=cols["GREN"], 
-     main="Group Reg. with/without tissue betas (Variables: "%+%nvars.PCBL%+%")")
+# create plot (5 vars)
+plot(roc.EN, type='l', col=cols["EN"], 
+     main="ROC comparison, blood data (variables: "%+%nvars.PCBL%+%")")
+points(roc.lasso, type='l', col=cols["lasso"])
 points(roc.GREN2, type='l', col=cols["GREN2"])
-legtext <- paste(c("With tissue betas, ", "Without, "),
+points(roc.GREN, type='l', col=cols["GREN"])
+legtext <- paste(c("Elastic Net, ", "Lasso, ", "Grp Reg. EN (counts only), ", "Grp Reg. EN (+ tissue betas), "),
                  "AUC: ",
-                 round(c(auc.GREN, auc.GREN2), 3),
+                 round(c(auc.EN, auc.lasso, auc.GREN2, auc.GREN), 2),
                  ", Sens: ",
-                 round(c(sens.GREN, sens.GREN2), 3),
+                 round(c(sens.EN, sens.lasso, sens.GREN2, sens.GREN), 2),
                  sep="")
 legend("bottomright", cex=0.8, lty=1,
        legend=legtext,
-       col=cols[c("GREN", "GREN2")],
+       col=cols[c("EN", "lasso", "GREN2", "GREN")],
        title="AUC, Sensitivity at specificity=0.9")
 dev.off()
+
+# png(filename="Diagrams/compare_ROC_PCBL_iso_with_without_tissue_beta_groups.png", width=800, height=400)
+# par(mfrow=c(1, 2))
+# plot(roc.GRR, type='l', col=cols["GREN"], #lwd=2,
+#      main="Group Reg.with/without tissue betas (All Variables)")
+# points(roc.GRR2, type='l', col=cols["GREN2"])
+# legtext <- paste(c("With tissue betas, ", "Without, "),
+#                  "AUC: ",
+#                  round(c(auc.GRR, auc.GRR2), 3),
+#                  ", Sens: ",
+#                  round(c(sens.GRR, sens.GRR2), 3),
+#                  sep="")
+# legend("bottomright", cex=0.8, lty=1,
+#        legend=legtext,
+#        col=cols[c("GREN", "GREN2")],
+#        title="AUC, Sensitivity at specificity=0.9")
+# 
+# plot(roc.GREN, type='l', col=cols["GREN"], 
+#      main="Group Reg. with/without tissue betas (Variables: "%+%nvars.PCBL%+%")")
+# points(roc.GREN2, type='l', col=cols["GREN2"])
+# legtext <- paste(c("With tissue betas, ", "Without, "),
+#                  "AUC: ",
+#                  round(c(auc.GREN, auc.GREN2), 3),
+#                  ", Sens: ",
+#                  round(c(sens.GREN, sens.GREN2), 3),
+#                  sep="")
+# legend("bottomright", cex=0.8, lty=1,
+#        legend=legtext,
+#        col=cols[c("GREN", "GREN2")],
+#        title="AUC, Sensitivity at specificity=0.9")
+# dev.off()
 
 rm(cols, X, Y, cvSets, cvo, p.ridge, auc.ridge, roc.ridge, sens.ridge, vars.EN, p.EN, auc.EN, roc.EN, sens.EN, vars.lasso, p.lasso, auc.lasso, roc.lasso, sens.lasso, vars.GREN, p.GREN, auc.GREN, roc.GREN, sens.GREN, vars.GREN2, p.GREN2, auc.GREN2, roc.GREN2, sens.GREN2, legtext)
